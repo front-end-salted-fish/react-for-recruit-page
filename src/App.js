64a4b71cf78578bc0461bfcb5bed274924123374
Chendabaiiii <532323969@qq.com>
2019-10-10 17:37:17 +0800

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 class App extends Component {
     render() {
         return (
-            <div>react</div>        
+            <div>reactttttttt</div>        
         )
     }
 }
